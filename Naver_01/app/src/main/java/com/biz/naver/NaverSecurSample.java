80ffa194367ca279530afcb8d4e6ecd48b694192
callor <callor@callor.com>
2020-02-24 09:21:24 +0900

    naver

---
+++ Naver_01/app/src/main/java/com/biz/naver/NaverSecurSample.java
@@ -0,0 +1,9 @@
+package com.biz.naver;
+
+public class NaverSecurSample {
+
+    public final static String NAVER_SEC = "YOUR NAVER SEC";
+    public final static String NAVER_ID = "YOUR NAVER ID";
+
+
+}
